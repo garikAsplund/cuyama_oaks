c556f7d1fbe0be2f3d0d70b456ffe43c886dca6b
Garik Asplund <garik.asplund@gmail.com>
2024-10-24 23:00:04 -0700

    mobile icons and maps

--- src/lib/PinButton.svelte
+++ src/lib/PinButton.svelte
@@ -1,20 +1,22 @@
 <script lang="ts">
 	import MapSvg from './MapSVG.svelte';
+	import { onMount } from 'svelte';
 
 	let directionsLink = '';
 
-	// Function to detect the device and set the directions link
 	function detectDevice() {
 		const userAgent = navigator.userAgent;
 
 		if (/iPad|iPhone|iPod/.test(userAgent)) {
-			directionsLink = "https://maps.apple.com/?ll=34.948958,-119.807372&q=Dropped%20Pin&t=m";
+			directionsLink = 'https://maps.apple.com/?ll=34.948958,-119.807372&q=Dropped%20Pin&t=m';
 		} else {
-			directionsLink = "https://www.google.com/maps/dir/?api=1&destination=Cuyama+Oaks+Ranch";
+			directionsLink = 'https://www.google.com/maps/dir/?api=1&destination=Cuyama+Oaks+Ranch';
 		}
 	}
 
-	detectDevice();
+	onMount(() => {
+		detectDevice();
+	});
 </script>
 
 <a
