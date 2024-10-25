c728c0555526622999cb8e61b8addb1573e05c91
Garik Asplund <garik.asplund@gmail.com>
2024-10-24 23:08:14 -0700

    fixed layering of insta button

--- src/lib/BookNowButton.svelte
+++ src/lib/BookNowButton.svelte
@@ -6,7 +6,7 @@
 	href="https://www.hipcamp.com/en-US/land/california-cuyama-oaks-ranch-ex9hp87z?adults=1&children=0&srid=5fd3d2e1-ab6c-41b1-89c9-40b2c4fc530a"
 	target="_blank"
 	rel="noopener"
-	class="absolute top-2/3 left-1/2 transform -translate-x-1/2 flex justify-center items-center space-x-3 text-xl hover:opacity-65 z-50 backdrop-blur-sm text-white opacity-80 px-4 py-2 border-2"
+	class="absolute top-2/3 left-1/2 transform -translate-x-1/2 flex justify-center items-center space-x-3 text-xl hover:opacity-65 z-50 backdrop-blur-sm text-white opacity-80 px-4 py-2 border-2 border-white"
 >
 	<CalendarSvg />
 	<p>Book Now</p>
