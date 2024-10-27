192994332e00cefa757536ceee0ed49c89bb0887
Garik Asplund <garik.asplund@gmail.com>
2024-10-26 19:26:59 -0700

    added address to footer with link

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -2,5 +2,7 @@
     <p>cuyamaoaksranch@gmail.com</p>
     <span>|</span>
     <p>(740) 258-8561</p>
+    <span>|</span>
+    <a href="https://maps.app.goo.gl/y3r2spJiP6Eptrir6" class="hover:opacity-80"><p>1900 Wasioja Rd, New Cuyama, CA</p></a>
   </footer>
   
