280ba68ca90f1a4a2131ee3069e94c71ecfaa101
Garik Asplund <garik.asplund@gmail.com>
2025-05-19 19:23:48 -0700

    updates from Nate

--- src/lib/campsites.ts
+++ src/lib/campsites.ts
@@ -6,15 +6,15 @@ export const campsites: {name: string, tag: string, link: string, description: s
       link: "https://www.hipcamp.com/en-US/land/california-cuyama-oaks-ranch-ex9hp87z/sites/400734?adults=1&children=0&srid=57601143-e41a-485c-917c-4f6c0d687c6c",
     },
     {
-      name: "Private Hilltop Canyon",
+      name: "Barbie Hilltop Camp",
       tag: "360° mountain views for groups of 4-8",
       description: "Rustic amenities: outdoor kitchen, hot shower, fire pit, and farm activities.",
       link: "https://www.hipcamp.com/en-US/land/california-cuyama-oaks-ranch-ex9hp87z/sites/400952?adults=1&children=0&srid=1bd9f34f-3a59-43e5-8c93-90ddd74a94a4",
     },
     {
-      name: "Happy Go Camping Sites",
-      tag: "Cozy sites for families or small groups",
-      description: "Shade cover, picnic area, grill, mini pool, composting toilet, and cookware provided.",
+      name: "Jolly Rancher Camp",
+      tag: "Cozy site for families or small groups",
+      description: "Shade cover, picnic area, grill, hot shower, mini pool, composting toilet, and cookware provided.",
       link: "https://www.hipcamp.com/en-US/land/california-cuyama-oaks-ranch-ex9hp87z/sites/404008?adults=1&children=0&srid=388faca8-0e6c-4c4e-a628-ea95de434955",
     }
   ];
